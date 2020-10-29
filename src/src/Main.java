a1e8da979c867eafa8893a865844b8003973ef98
DILARA BOZYILAN <dilarabozyilan@gmail.com>
2020-10-29 17:52:25 +0300

    Initial commit- client master

---
+++ src/src/Main.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        src.ConnectionToServer connectionToServer = new ConnectionToServer(ConnectionToServer.DEFAULT_SERVER_ADDRESS, ConnectionToServer.DEFAULT_SERVER_PORT);
+        connectionToServer.Connect();
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("Enter a message for the echo");
+        String message = "";
+
+        System.out.println("Response from server: " +connectionToServer.getServerOutput());
+
+        while (!message.equals("QUIT"))
+        {
+            message = scanner.nextLine();
+            System.out.println("Response from server: " + connectionToServer.SendForAnswer(message));
+
+        }
+        connectionToServer.Disconnect();
+    }
+}
